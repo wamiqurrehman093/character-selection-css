585e52e0c550008bfcd38c92658a067d29b95298
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2023-09-05 23:15:35 +0500

    Handling Touch Input and Detecting OS

--- src/guide.js
+++ src/guide.js
@@ -1,10 +1,19 @@
-var modal = document.getElementById("guide-modal");
+var modal = document.getElementById("controls-guide-modal");
+modal.style.display = "block";
 
-var closeButton = modal.getElementsByClassName("close")[0];
+if (window.DocumentTouch && document instanceof DocumentTouch) {
+    document.getElementById("mouse-controls-guide").style.display = "block";
+} else {
+    document.getElementById("keyboard-controls-guide").style.display = "block";
+}
 
-closeButton.onclick = function () {
-    modal.style.display = "none";
-    timer = setInterval(updateTimer, 1000)
+var closeButtons = modal.getElementsByClassName("close");
+
+for (var i = 0; i < closeButtons.length; i++){
+    closeButtons[i].onclick = function () {
+        modal.style.display = "none";
+        timer = setInterval(updateTimer, 1000)
+    }
 }
 
 window.onclick = function (event) {
@@ -12,4 +21,27 @@ window.onclick = function (event) {
         modal.style.display = "none";
         timer = setInterval(updateTimer, 1000)
     }
+}
+
+function getOS() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+    } else if (/Linux/.test(platform)) {
+        os = 'Linux';
+    }
+
+    return os;
 }
